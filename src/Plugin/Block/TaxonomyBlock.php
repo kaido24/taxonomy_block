70af45a2fa81e3d080a9a11d129b2cfbf0c52dab
Kaido <kaido@web3.ee>
2018-11-12 17:40:10 +0200

    Update TaxonomyBlock.php

--- src/Plugin/Block/TaxonomyBlock.php
+++ src/Plugin/Block/TaxonomyBlock.php
@@ -51,13 +51,45 @@ class TaxonomyBlock extends BlockBase {
     $tt = \Drupal::service('taxonomy_block.taxonomy_term_tree');
     $tree = $tt->load($vid);
 
+    $route_match = \Drupal::service('current_route_match');
+    $active_term = $route_match->getParameter('taxonomy_term');
+    $active_build = 0;
+    $parents = array();
+
+    if($active_term != NULL){
+      $active_build = $active_term->id();
+      $parent = $this->getParents($active_term->id());
+      while($parent[0] != 0) {
+        $parents[$parent[0]] = $parent[0];
+        $parent = $this->getParents($parent[0]);
+      }
+      //ksm($parents);
+    }
+
     $build = [];
     $build['tb'] = [
       '#theme' => 'taxonomy_block',
       '#terms' => $tree,
+      '#active' => $active_build,
+      '#parents' => $parents
     ];
     return $build;
   }
+
+  /**
+   *
+   * @param $element_id
+   * @return $parent_id
+   */
+  public function getParents($element_id) {
+    $query = \Drupal::database()->select('taxonomy_term__parent', 't');
+    $query->addField('t','parent_target_id');
+    $query->condition('entity_id', $element_id, "=");
+    $parent_id = $query->execute()->fetchCol();
+    return $parent_id;
+  }
+
+
   /**
    * {@inheritdoc}
    */
